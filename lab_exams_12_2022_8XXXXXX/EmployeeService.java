0a22c40a977fdeef9202adf825146105bb986f59
Radu Scobioala <scobioalachrist@gmail.com>
2024-12-07 21:21:53 +0200

    Create service for business logic behind employee data update and retrieval

---
+++ lab_exams_12_2022_8XXXXXX/EmployeeService.java
@@ -0,0 +1,29 @@
+package lab_exams_12_2022_8XXXXXX;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeService {
+
+        /**
+         * Search employee by keyword
+         * 
+         * @param keyword, String
+         * @return List<Employee>
+         * @throws Exception
+         */
+        public List<Employee> searchEmployees(String keyword) throws Exception {
+                // ΘΕΜΑ 1Α
+        }
+
+        /**
+         * Register/create new Employee.
+         * 
+         * @param employee, employee
+         * @throws Exception, if encounter any error.
+         */
+        public void registerEmployee(Employee employee) throws Exception {
+                // ΘΕΜΑ 1Β
+        }
+}
