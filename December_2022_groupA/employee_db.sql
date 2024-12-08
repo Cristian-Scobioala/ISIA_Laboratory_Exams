5586b25464fbe443e0182bbba685635455847ba1
Radu Scobioala <scobioalachrist@gmail.com>
2024-12-08 16:18:47 +0200

    Correct column name in SQL query for table creation and data insertion

--- December_2022_groupA/employee_db.sql
+++ December_2022_groupA/employee_db.sql
@@ -1,15 +1,15 @@
-CREATE TABLE `employee_exam_12_2024_8xxxxxx` (
+CREATE TABLE `employee_exam_12_2022_8xxxxxx` (
   `afm` varchar(45) NOT NULL,
   `firstname` varchar(45) NOT NULL,
   `lastname` varchar(45) NOT NULL,
   `salary` double NOT NULL,
-  `is_external_collaborator` TINYINT(1) NOT NULL,
+  `external_partner` TINYINT(1) NOT NULL,
   PRIMARY KEY (`afm`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-INSERT INTO `employee_exam_12_2024_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`is_external_collaborator`) VALUES ('444444444','Lorin','Allitt',1910, 0);
-INSERT INTO `employee_exam_12_2024_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`is_external_collaborator`) VALUES ('111111111','Holly','Chant',1300, 0);
-INSERT INTO `employee_exam_12_2024_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`is_external_collaborator`) VALUES ('555555555','Alan','Duly',2050, 0);
-INSERT INTO `employee_exam_12_2024_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`is_external_collaborator`) VALUES ('222222222','Rita','Jewer',1640, 1);
-INSERT INTO `employee_exam_12_2024_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`is_external_collaborator`) VALUES ('333333333','Alan','Lancastle',940, 1);
+INSERT INTO `employee_exam_12_2022_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`external_partner`) VALUES ('444444444','Lorin','Allitt',1910, 0);
+INSERT INTO `employee_exam_12_2022_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`external_partner`) VALUES ('111111111','Holly','Chant',1300, 0);
+INSERT INTO `employee_exam_12_2022_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`external_partner`) VALUES ('555555555','Alan','Duly',2050, 0);
+INSERT INTO `employee_exam_12_2022_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`external_partner`) VALUES ('222222222','Rita','Jewer',1640, 1);
+INSERT INTO `employee_exam_12_2022_8xxxxxx` (`afm`,`firstname`,`lastname`,`salary`,`external_partner`) VALUES ('333333333','Alan','Lancastle',940, 1);
