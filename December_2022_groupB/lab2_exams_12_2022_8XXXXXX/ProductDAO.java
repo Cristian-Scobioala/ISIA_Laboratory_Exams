7b4ad44140b6bb740f85156255b0e0b22c958be1
Dionisia <t8220066@aueb.gr>
2024-12-08 14:54:44 +0200

    Add some of the .java files for the exams of December 2022 groupB

---
+++ December_2022_groupB/lab2_exams_12_2022_8XXXXXX/ProductDAO.java
@@ -0,0 +1,29 @@
+package lab2_exams_12_2022_8XXXXXX;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductDAO {
+
+        /**
+         * Search product by keyword
+         * 
+         * @param keyword, String
+         * @return List<Product>
+         * @throws Exception
+         */
+        public List<Product> searchProduct(String keyword) throws Exception {
+                // ΘΕΜΑ 1Α
+        }
+
+        /**
+         * Register/create new Employee.
+         * 
+         * @param product, product
+         * @throws Exception, if encounter any error.
+         */
+        public void registerProduct(Product product) throws Exception {
+                // ΘΕΜΑ 1Β
+        }
+}
