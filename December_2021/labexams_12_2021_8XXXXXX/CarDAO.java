722d8a54f6535b2338204ef2ea8bd40f2de20a98
Radu Scobioala <scobioalachrist@gmail.com>
2024-12-08 09:31:02 +0200

    Create business logic class for retrieving data regarding cars

---
+++ December_2021/labexams_12_2021_8XXXXXX/CarDAO.java
@@ -0,0 +1,33 @@
+package December_2021.labexams_12_2021_8XXXXXX;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.sql.*;
+
+public class CarDAO {
+
+        /**
+         * Search car by keyword
+         * 
+         * @param keyword, String
+         * @return List<Car>
+         * @throws Exception
+         */
+        public List<Car> findCars(String keyword) throws Exception {
+                // ΘΕΜΑ 1Α
+        }
+
+        /**
+         * Search user by carCode
+         * 
+         * @param carCode, String
+         * @return Car, the Car object
+         * @throws Exception
+         */
+        public Car getCarByCode(String carCode) throws Exception {
+                // ΘΕΜΑ 1Β
+        }
+
+        public List<Car>
+
+}
