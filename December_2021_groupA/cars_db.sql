8b620334036890e1dfca70b87001ab3c02488217
Radu Scobioala <scobioalachrist@gmail.com>
2024-12-08 16:54:37 +0200

    Correct sql query for cars

--- December_2021_groupA/cars_db.sql
+++ December_2021_groupA/cars_db.sql
@@ -1,13 +1,13 @@
 CREATE TABLE `cars_exam_12_2021_8xxxxxx` (
-        `carCode` varchar(45) NOT NULL,
-        `firm` varchar(45) NOT NULL,
+        `code` varchar(45) NOT NULL,
+        `brand` varchar(45) NOT NULL,
         `model` varchar(45) NOT NULL,
         `year` int NOT NULL,
         `has_discount` TINYINT(1) NOT NULL,
         `is_new`  TINYINT(1) NOT NULL,
-        PRIMARY KEY (`carCode`),
+        PRIMARY KEY (`code`),
         UNIQUE KEY `model_UNIQUE` (`model`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `cars_exam_12_2021_8xxxxxx` (`carCode`, `firm`, `model`, `year`, `has_discount`, `is_new`) VALUES ('4F2CY0C7XBK810614', 'BMW', '118i', 2020, 1, 1)
-INSERT INTO `cars_exam_12_2021_8xxxxxx` (`carCode`, `firm`, `model`, `year`, `has_discount`, `is_new`) VALUES ('3VW4A7AT6DM445536', 'BMW', 'i4', 2019, 0, 0)
+INSERT INTO `cars_exam_12_2021_8xxxxxx` (`code`, `brand`, `model`, `year`, `has_discount`, `is_new`) VALUES ('4F2CY0C7XBK810614', 'BMW', '118i', 2020, 1, 1);
+INSERT INTO `cars_exam_12_2021_8xxxxxx` (`code`, `brand`, `model`, `year`, `has_discount`, `is_new`) VALUES ('3VW4A7AT6DM445536', 'BMW', 'i4', 2019, 0, 0);
